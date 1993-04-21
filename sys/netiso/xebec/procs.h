8e7ec549b3b4feb683bedb56512c91c27a4db3fe
cvs2git <cvs2git@openbsd.org>
1993-04-21 04:51:09 +0000

    Initialize CVS branch BSD
    The per-file CVS branch roots do not exactly match a reconstructed source-branch commit.

--- sys/netiso/xebec/procs.h
+++
@@ -1,5 +0,0 @@
-/* $Header: /home/mike/src/cvs/netbsd/src/sys/netiso/xebec/Attic/procs.h,v 1.1 1993/04/09 12:02:14 cgd Exp $ */
-/* $Source: /home/mike/src/cvs/netbsd/src/sys/netiso/xebec/Attic/procs.h,v $ */
-
-extern char *stash();
-extern struct Object *SameState;
