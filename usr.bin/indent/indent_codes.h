98e3570b377df7655dac728a213864cf12d91bc5
mrg <mrg@netbsd.org>
1993-06-06 21:52:48 +0000

    4.4BSD-Lite2

--- usr.bin/indent/indent_codes.h
+++ usr.bin/indent/indent_codes.h
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 1989, 1993
+ * Copyright (c) 1985 Sun Microsystems, Inc.
+ * Copyright (c) 1980, 1993
  *	The Regents of the University of California.  All rights reserved.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -29,100 +31,39 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ *	@(#)indent_codes.h	8.1 (Berkeley) 6/6/93
  */
 
-#ifndef lint
-static char sccsid[] = "@(#)conv.c	8.1 (Berkeley) 6/6/93";
-#endif /* not lint */
-
-#include <sys/types.h>
-
-#include <stdio.h>
-#include <ctype.h>
-#include "hexdump.h"
-
-void
-conv_c(pr, p)
-	PR *pr;
-	u_char *p;
-{
-	extern int deprecated;
-	char buf[10], *str;
-
-	switch(*p) {
-	case '\0':
-		str = "\\0";
-		goto strpr;
-	/* case '\a': */
-	case '\007':
-		if (deprecated)		/* od didn't know about \a */
-			break;
-		str = "\\a";
-		goto strpr;
-	case '\b':
-		str = "\\b";
-		goto strpr;
-	case '\f':
-		str = "\\f";
-		goto strpr;
-	case '\n':
-		str = "\\n";
-		goto strpr;
-	case '\r':
-		str = "\\r";
-		goto strpr;
-	case '\t':
-		str = "\\t";
-		goto strpr;
-	case '\v':
-		if (deprecated)
-			break;
-		str = "\\v";
-		goto strpr;
-	default:
-		break;
-	}
-	if (isprint(*p)) {
-		*pr->cchar = 'c';
-		(void)printf(pr->fmt, *p);
-	} else {
-		(void)sprintf(str = buf, "%03o", (int)*p);
-strpr:		*pr->cchar = 's';
-		(void)printf(pr->fmt, str);
-	}
-}
-
-void
-conv_u(pr, p)
-	PR *pr;
-	u_char *p;
-{
-	extern int deprecated;
-	static char *list[] = {
-		"nul", "soh", "stx", "etx", "eot", "enq", "ack", "bel",
-		 "bs",  "ht",  "lf",  "vt",  "ff",  "cr",  "so",  "si",
-		"dle", "dcl", "dc2", "dc3", "dc4", "nak", "syn", "etb",
-		"can",  "em", "sub", "esc",  "fs",  "gs",  "rs",  "us",
-	};
-
-						/* od used nl, not lf */
-	if (*p <= 0x1f) {
-		*pr->cchar = 's';
-		if (deprecated && *p == 0x0a)
-			(void)printf(pr->fmt, "nl");
-		else
-			(void)printf(pr->fmt, list[*p]);
-	} else if (*p == 0x7f) {
-		*pr->cchar = 's';
-		(void)printf(pr->fmt, "del");
-	} else if (deprecated && *p == 0x20) {	/* od replaced space with sp */
-		*pr->cchar = 's';
-		(void)printf(pr->fmt, " sp");
-	} else if (isprint(*p)) {
-		*pr->cchar = 'c';
-		(void)printf(pr->fmt, *p);
-	} else {
-		*pr->cchar = 'x';
-		(void)printf(pr->fmt, (int)*p);
-	}
-}
+#define newline		1
+#define lparen		2
+#define rparen		3
+#define unary_op	4
+#define binary_op	5
+#define postop		6
+#define question	7
+#define casestmt	8
+#define colon		9
+#define semicolon	10
+#define lbrace		11
+#define rbrace		12
+#define ident		13
+#define comma		14
+#define comment		15
+#define swstmt		16
+#define preesc		17
+#define form_feed	18
+#define decl		19
+#define sp_paren	20
+#define sp_nparen	21
+#define ifstmt		22
+#define whilestmt	23
+#define forstmt		24
+#define stmt		25
+#define stmtl		26
+#define elselit		27
+#define dolit		28
+#define dohead		29
+#define ifhead		30
+#define elsehead	31
+#define period		32
