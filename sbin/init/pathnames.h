ca2477408c8395da73605605967ee11fde9cbf47
mycroft <mycroft@netbsd.org>
1994-06-13 23:56:00 +0000

    Import 4.4-Lite version.

--- sbin/init/pathnames.h
+++ sbin/init/pathnames.h
@@ -1,9 +1,9 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
- * Donn Seeley at UUNET Technologies, Inc.
+ * Donn Seeley at Berkeley Software Design, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,8 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	from: @(#)pathnames.h	6.1 (Berkeley) 7/2/91
- *	$Id: pathnames.h,v 1.1 1993/06/18 20:51:50 cgd Exp $
+ *	@(#)pathnames.h	8.1 (Berkeley) 6/5/93
  */
 
 #include <paths.h>
