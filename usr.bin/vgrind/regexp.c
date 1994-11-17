ae80abb75650bd33f459f7dd852b187ae5049bfd
jtc <jtc@netbsd.org>
1994-11-17 08:09:55 +0000

    imported from 4.4lite

--- usr.bin/vgrind/regexp.c
+++ usr.bin/vgrind/regexp.c
@@ -1,6 +1,7 @@
 /*
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,17 +33,29 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)regexp.c	5.3 (Berkeley) 6/1/90";
+static char copyright[] =
+"@(#) Copyright (c) 1980, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)regexp.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 #include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include "extern.h"
 
-typedef int	boolean;
-#define TRUE	1
 #define FALSE	0
+#define TRUE	!(FALSE)
 #define NIL	0
 
-boolean l_onecase;	/* true if upper and lower equivalent */
+static void	expconv __P((void));
+
+boolean	 _escaped;	/* true if we are currently _escaped */
+char	*_start;	/* start of string */
+boolean	 l_onecase;	/* true if upper and lower equivalent */
 
 #define makelower(c) (isupper((c)) ? tolower((c)) : (c))
 
@@ -51,6 +64,7 @@ boolean l_onecase;	/* true if upper and lower equivalent */
  *		if l_onecase is set.
  */
 
+int
 STRNCMP(s1, s2, len)
 	register char *s1,*s2;
 	register int len;
@@ -129,9 +143,8 @@ STRNCMP(s1, s2, len)
 #define ALT 8
 #define OPER 16
 
-char *ure;		/* pointer current position in unconverted exp */
-char *ccre;		/* pointer to current position in converted exp*/
-char *malloc();
+static char *ccre;	/* pointer to current position in converted exp*/
+static char *ure;	/* pointer current position in unconverted exp */
 
 char *
 convexp(re)
@@ -159,6 +172,7 @@ convexp(re)
     return (cre);
 }
 
+static void
 expconv()
 {
     register char *cs;		/* pointer to current symbol in converted exp */
@@ -249,7 +263,7 @@ expconv()
 	    OCNT(cs) = ccre - cs;		/* offset to next symbol */
 	    break;
 
-	/* return from a recursion */
+	/* reurn from a recursion */
 	case ')':
 	    if (acs != NIL) {
 		do {
@@ -328,9 +342,6 @@ expconv()
  *	The value returned is the pointer to the first non \a 
  *	character matched.
  */
-
-boolean _escaped;		/* true if we are currently _escaped */
-char *_start;			/* start of string */
 
 char *
 expmatch (s, re, mstring)
--- usr.bin/vgrind/vgrind.sh
+++ usr.bin/vgrind/vgrind.sh
@@ -1,7 +1,7 @@
 #!/bin/csh -f
 #
-# Copyright (c) 1980 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1980, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -31,7 +31,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-#	@(#)vgrind.sh	5.7 (Berkeley) 4/16/91
+#       @(#)vgrind.sh	8.1 (Berkeley) 6/6/93
 #
 
 set voptions=
@@ -116,10 +116,10 @@ if (-r index) then
     else
 	if ("$head" != "") then
 	    $vf $options -h "$head" $files | \
-		sh -c "vtroff -rx1 $voptions -i -mvgrind 2>> xindex"
+		sh -c "psroff -rx1 $voptions -i -mvgrind 2>> xindex"
 	else
 	    $vf $options $files | \
-		sh -c "vtroff -rx1 $voptions -i -mvgrind 2>> xindex"
+		sh -c "psroff -rx1 $voptions -i -mvgrind 2>> xindex"
 	endif
     endif
     sort -df +0 -2 xindex >index
@@ -133,9 +133,9 @@ else
 	endif
     else
 	if ("$head" != "") then
-	    $vf $options -h "$head" $files | vtroff -i $voptions -mvgrind
+	    $vf $options -h "$head" $files | psroff -i $voptions -mvgrind
 	else
-	    $vf $options $files | vtroff -i $voptions -mvgrind
+	    $vf $options $files | psroff -i $voptions -mvgrind
 	endif
     endif
 endif
