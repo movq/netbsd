3bd71f9adf55c2de290c72c9745845fa69dcc44c
cvs2git <cvs2git@openbsd.org>
1994-12-01 04:26:47 +0000

    Initialize CVS branch GNU-dc-1-03
    The per-file CVS branch roots do not exactly match a reconstructed source-branch commit.

--- sys/arch/sun3/dev/if_le_subr.h
+++
@@ -1,5 +0,0 @@
-/*	$NetBSD: if_le_subr.h,v 1.5 1994/11/21 21:30:56 gwr Exp $	*/
-
-/* One might also set: LE_C3_ACON | LE_C3_BCON */
-#define	LE_C3_CONFIG LE_C3_BSWP
-
--- sys/arch/sun3/include/db_machdep.h
+++
@@ -1,3 +0,0 @@
-/*	$NetBSD: db_machdep.h,v 1.6 1994/11/21 21:33:34 gwr Exp $	*/
-
-#include <m68k/db_machdep.h>
