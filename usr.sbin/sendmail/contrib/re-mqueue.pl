ca3ff08f7e892a794b1188713192ae4554ae59d3
mrg <mrg@netbsd.org>
1996-11-18 04:31:56 +0000

    sendmail 8.8.3

--- usr.sbin/sendmail/contrib/re-mqueue.pl
+++ usr.sbin/sendmail/contrib/re-mqueue.pl
@@ -84,7 +84,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# @(#)$Id: re-mqueue.pl,v 1.1 1996/11/18 04:31:56 mrg Exp $
+# @(#)$Id: re-mqueue.pl,v 1.1.1.1 1996/11/18 04:31:56 mrg Exp $
 
 require "syslog.pl";
 
