b3b9e60b2ef8514514172df0d4826739efa8781d
mikel <mikel@netbsd.org>
1996-12-10 07:18:55 +0000

    Upgrade to flex 2.5.4.

--- usr.bin/lex/dfa.c
+++ usr.bin/lex/dfa.c
@@ -26,7 +26,7 @@
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
 
-/* $Header: /home/mike/src/cvs/netbsd/src/usr.bin/lex/Attic/dfa.c,v 1.9 1995/05/05 05:35:14 jtc Exp $ */
+/* $Header: /home/mike/src/cvs/netbsd/src/usr.bin/lex/Attic/dfa.c,v 1.10 1996/12/10 07:18:40 mikel Exp $ */
 
 #include "flexdef.h"
 
