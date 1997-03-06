2d33bc0846ba2ffa64d7db675036a9e25914f5f4
mikel <mikel@netbsd.org>
1997-03-06 05:27:18 +0000

    import 4.4BSD-Lite2 version

--- usr.sbin/accton/accton.c
+++ usr.sbin/accton/accton.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,28 +32,64 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1988 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1988, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)accton.c	4.3 (Berkeley) 6/1/90";
+static char sccsid[] = "@(#)accton.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
+#include <sys/types.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
+void usage __P((void));
+
+int
 main(argc, argv)
 	int argc;
-	char **argv;
+	char *argv[];
 {
-	if (argc > 2) {
-		fputs("usage: accton [file]\n", stderr);
-		exit(1);
-	}
-	if (acct(argc == 2 ? argv[1] : (char *)NULL)) {
-		perror("accton");
-		exit(1);
+	int ch;
+
+	while ((ch = getopt(argc, argv, "")) != EOF)
+		switch(ch) {
+		case '?':
+		default:
+			usage();
+		}
+	argc -= optind;
+	argv += optind;
+
+	switch(argc) {
+	case 0: 
+		if (acct(NULL)) {
+			(void)fprintf(stderr,
+			    "accton: %s\n", strerror(errno));
+			exit(1);
+		}
+		break;
+	case 1:
+		if (acct(*argv)) {
+			(void)fprintf(stderr,
+			    "accton: %s: %s\n", *argv, strerror(errno));
+			exit(1);
+		}
+		break;
+	default:
+		usage();
 	}
 	exit(0);
+}
+
+void
+usage()
+{
+	(void)fprintf(stderr, "usage: accton [file]\n");
+	exit(1);
 }
