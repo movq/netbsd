c17b7bbbc2e43d665ca31134c7ccc707a1f2eccc
thorpej <thorpej@netbsd.org>
1997-10-14 08:26:52 +0000

    Update marc-pcmcia branch from trunk.

--- sys/arch/amiga/dev/bzivscvar.h
+++ sys/arch/amiga/dev/bzivscvar.h
@@ -1,7 +1,8 @@
-/*	$NetBSD: flscreg.h,v 1.2 1996/04/21 21:11:04 veego Exp $	*/
+/*	$NetBSD: bzivscvar.h,v 1.1.2.2 1997/10/14 08:26:11 thorpej Exp $	*/
 
 /*
- * Copyright (c) 1995 Daniel Widenfalk
+ * Copyright (c) 1997 Michael L. Hitch.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -13,10 +14,10 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *      This product includes software developed by Daniel Widenfalk
- *      for the NetBSD Project.
+ *	This product includes software developed for the NetBSD Project
+ *	by Michael L. Hitch.
  * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
+ *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -29,39 +30,29 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-/*
- * FastlaneZ3 with FAS216 SCSI interface hardware description.
- */
 
-#ifndef _FLSCREG_H_
-#define _FLSCREG_H_
+struct bzivsc_softc {
+	struct ncr53c9x_softc	sc_ncr53c9x;	/* glue to MI code */
 
-typedef struct flsc_regmap {
-	sfas_regmap_t	FAS216;
-	vu_char		*hardbits;
-	vu_char		*clear;
-	vu_char		*dmabase;
-} flsc_regmap_t;
-typedef flsc_regmap_t *flsc_regmap_p;
+	struct	isr		 sc_isr;	/* Interrupt chain struct */
 
-#define FLSC_HB_DISABLED	0x01
-#define FLSC_HB_BUSID6		0x02
-#define FLSC_HB_SEAGATE		0x04
-#define FLSC_HB_SLOW		0x08
-#define FLSC_HB_SYNCHRON	0x10
-#define FLSC_HB_CREQ		0x20
-#define FLSC_HB_IACT		0x40
-#define FLSC_HB_MINT		0x80
+	volatile u_char *sc_reg;		/* the registers */
+	volatile u_char *sc_dmabase;
 
-#define FLSC_PB_ESI		0x01
-#define FLSC_PB_EDI		0x02
-#define FLSC_PB_ENABLE_DMA	0x04
-#define FLSC_PB_DISABLE_DMA	0x00	/* Symmetric reasons */
-#define FLSC_PB_DMA_WRITE	0x08
-#define FLSC_PB_DMA_READ	0x00	/* Symmetric reasons */
-#define FLSC_PB_LED		0x10
+	int		sc_active;		/* Pseudo-DMA state vars */
+	int		sc_datain;
+	int		sc_tc;
+	size_t		sc_dmasize;
+	size_t		sc_dmatrans;
+	char		**sc_dmaaddr;
+	size_t		*sc_pdmalen;
+	vm_offset_t	sc_pa;
 
-#define FLSC_PB_INT_BITS (FLSC_PB_ESI | FLSC_PB_EDI)
-#define FLSC_PB_DMA_BITS (FLSC_PB_ENABLE_DMA | FLSC_PB_DMA_WRITE)
+	u_char		sc_pad1[18];		/* XXX */
+	u_char		sc_alignbuf[256];
+	u_char		sc_pad2[16];
+	u_char		sc_hardbits;
+	u_char		sc_portbits;
+	u_char		sc_xfr_align;
 
-#endif
+};
--- sys/arch/amiga/dev/bztzscvar.h
+++ sys/arch/amiga/dev/bztzscvar.h
@@ -1,8 +1,8 @@
-/*	$NetBSD: bztzscvar.h,v 1.1 1996/12/16 16:17:30 is Exp $	*/
+/*	$NetBSD: bztzscvar.h,v 1.1.10.1 1997/10/14 08:26:20 thorpej Exp $	*/
 
 /*
- * Copyright (c) 1996 Ignatios Souvatzis
- * Copyright (c) 1995 Daniel Widenfalk
+ * Copyright (c) 1997 Michael L. Hitch.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -14,10 +14,10 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *      This product includes software developed by Daniel Widenfalk
- *      for the NetBSD Project.
+ *	This product includes software developed for the NetBSD Project
+ *	by Michael L. Hitch.
  * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
+ *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -30,20 +30,31 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef _BZTZSCVAR_H_
-#define _BZTZSCVAR_H_
 
-#ifndef _SFASVAR_H_
-#include <amiga/dev/sfasvar.h>
-#endif
+struct bztzsc_softc {
+	struct ncr53c9x_softc	sc_ncr53c9x;	/* glue to MI code */
 
-#ifndef _BZTZSCREG_H_
-#include <amiga/dev/bztzscreg.h>
-#endif
+	struct	isr		 sc_isr;	/* Interrupt chain struct */
 
-struct	bztzsc_softc {
-	struct sfas_softc	sc_softc;
-	bztzsc_regmap_t		sc_regmap;
+	volatile u_char *sc_reg;		/* the registers */
+	volatile u_char *sc_dmabase;
+
+	int		sc_active;		/* Pseudo-DMA state vars */
+	int		sc_datain;
+	int		sc_tc;
+	size_t		sc_dmasize;
+	size_t		sc_dmatrans;
+	char		**sc_dmaaddr;
+	size_t		*sc_pdmalen;
+	vm_offset_t	sc_pa;
+
+	u_char		sc_pad1[18];		/* XXX */
+	u_char		sc_alignbuf[256];
+	u_char		sc_pad2[16];
+	u_char		sc_hardbits;
+	u_char		sc_portbits;
+	u_char		sc_xfr_align;
+
 };
 
-#endif /* _BZTZSCVAR_H_ */
+#define BZTZSC_PB_LED		0x02		/* clear to turn LED on */
