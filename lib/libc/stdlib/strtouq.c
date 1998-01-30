d3336e08c2f7648a9ef36dff00c5ed31caaef7ec
perry <perry@netbsd.org>
1998-01-30 21:42:16 +0000

    import lite-2

--- lib/libc/stdlib/strtouq.c
+++ lib/libc/stdlib/strtouq.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strtouq.c	5.1 (Berkeley) 6/26/92";
+static char sccsid[] = "@(#)strtouq.c	8.1 (Berkeley) 6/4/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -109,6 +109,6 @@ strtouq(nptr, endptr, base)
 	} else if (neg)
 		acc = -acc;
 	if (endptr != 0)
-		*endptr = any ? s - 1 : (char *)nptr;
+		*endptr = (char *)(any ? s - 1 : nptr);
 	return (acc);
 }
