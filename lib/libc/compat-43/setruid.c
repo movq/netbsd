cf4317ae41acecd0927924748b0f86d4b0d232a8
perry <perry@netbsd.org>
1998-02-02 06:08:02 +0000

    import lite-2

--- lib/libc/compat-43/setruid.c
+++ lib/libc/compat-43/setruid.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,8 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-/*static char *sccsid = "from: @(#)setruid.c	5.5 (Berkeley) 2/23/91";*/
-static char *rcsid = "$Id: setruid.c,v 1.1 1994/04/06 23:28:48 cgd Exp $";
+static char sccsid[] = "@(#)setruid.c	8.1 (Berkeley) 6/2/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <unistd.h>
