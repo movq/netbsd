452885d26414c8aa84cc277430d0690543aac693
mellon <mellon@netbsd.org>
2000-06-10 18:05:38 +0000

    Belated import of recent DHCP core dumps, etc.   Approved by releng.

--- usr.sbin/dhcp/common/comapi.c
+++ usr.sbin/dhcp/common/comapi.c
@@ -50,7 +50,7 @@
 
 #ifndef lint
 static char copyright[] =
-"$Id: comapi.c,v 1.1 2000/06/10 18:04:43 mellon Exp $ Copyright (c) 1999-2000 The Internet Software Consortium.  All rights reserved.\n";
+"$Id: comapi.c,v 1.1.1.1 2000/06/10 18:04:43 mellon Exp $ Copyright (c) 1999-2000 The Internet Software Consortium.  All rights reserved.\n";
 #endif /* not lint */
 
 #include "dhcpd.h"
