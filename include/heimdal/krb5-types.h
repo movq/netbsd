ea7f35b7324c885942ace42097637a88e3554d6a
thorpej <thorpej@netbsd.org>
2000-06-16 19:18:31 +0000

    Import Heimdal build-helper includes from cryptosrc-intl.

--- include/heimdal/krb5-types.h
+++ include/heimdal/krb5-types.h
@@ -1,5 +1,5 @@
 /* krb5-types.h -- this file was generated for i386-unknown-netbsd1.4P by
-                   $Id: krb5-types.h,v 1.1 2000/06/16 19:18:30 thorpej Exp $ */
+                   $Id: krb5-types.h,v 1.1.1.1 2000/06/16 19:18:30 thorpej Exp $ */
 
 #ifndef __krb5_types_h__
 #define __krb5_types_h__
