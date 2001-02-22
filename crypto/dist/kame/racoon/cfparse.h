fddeca3730225574f95fd2461a4087910da4a3e6
itojun <itojun@netbsd.org>
2001-02-22 02:23:28 +0000

    KAME racoon as of 2001/2/22

--- crypto/dist/kame/racoon/cfparse.h
+++ crypto/dist/kame/racoon/cfparse.h
@@ -1,4 +1,4 @@
-/*	$KAME: cfparse.h,v 1.7 2000/10/04 17:40:58 itojun Exp $	*/
+/*	$KAME: cfparse.h,v 1.10 2001/01/31 05:45:23 sakane Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -28,19 +28,6 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-
-#define DEFAULT_CF_FILE	SYSCONFDIR "/racoon.conf"
-
-#define CF_PATHTYPE_INCLUDE	0
-#define CF_PATHTYPE_PSK		1
-#define CF_PATHTYPE_CERT	2
-#define CF_PATHTYPE_MAX		3
-
-#define CF_PLADDR_ASSOCIATION	0
-#define CF_PLADDR_NEGOTIATION	1
-
-#define CF_SIDE_LOCAL		0
-#define CF_SIDE_REMOTE		1
 
 #define CF_LIFETYPE_TIME	0
 #define CF_LIFETYPE_BYTE	1
