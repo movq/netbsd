7a1fb40772f889e1c38f8a94110651fbf18b0fc6
itojun <itojun@netbsd.org>
2003-07-24 08:29:48 +0000

    OpenSSL 0.9.7b, major API changes included

--- crypto/dist/openssl/crypto/engine/vendor_defns/hwcryptohook.h
+++ crypto/dist/openssl/crypto/engine/vendor_defns/hwcryptohook.h
@@ -65,7 +65,7 @@
  * please contact nCipher.
  *
  *
- * $Id: hwcryptohook.h,v 1.1 2003/07/24 08:27:25 itojun Exp $
+ * $Id: hwcryptohook.h,v 1.1.1.1 2003/07/24 08:27:25 itojun Exp $
  */
 
 #ifndef HWCRYPTOHOOK_H
