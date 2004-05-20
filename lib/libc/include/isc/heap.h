749bc6f6724f0b48660b7a7266ea6b8d2f5e45dd
christos <christos@netbsd.org>
2004-05-20 19:49:41 +0000

    import isc specific header files.

--- lib/libc/include/isc/heap.h
+++ lib/libc/include/isc/heap.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: heap.h,v 1.1 2004/05/20 19:49:41 christos Exp $	*/
+/*	$NetBSD: heap.h,v 1.1.1.1 2004/05/20 19:49:41 christos Exp $	*/
 
 /*
  * Copyright (c) 2004 by Internet Systems Consortium, Inc. ("ISC")
--- lib/libc/include/isc/memcluster.h
+++ lib/libc/include/isc/memcluster.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: memcluster.h,v 1.1 2004/05/20 19:49:41 christos Exp $	*/
+/*	$NetBSD: memcluster.h,v 1.1.1.1 2004/05/20 19:49:41 christos Exp $	*/
 
 /*
  * Copyright (c) 2004 by Internet Systems Consortium, Inc. ("ISC")
