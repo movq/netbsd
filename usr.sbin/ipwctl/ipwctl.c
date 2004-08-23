adb7d3487859d299297312ae766821e48aa32b76
lukem <lukem@netbsd.org>
2004-08-23 11:46:18 +0000

    ipwctl(8) - configure Intel(R) PRO/Wireless 2100 (ipw(4)) network adapter Written by Damien Bergamini, from http://damien.bergamini.free.fr/ipw/

--- usr.sbin/ipwctl/ipwctl.c
+++ usr.sbin/ipwctl/ipwctl.c
@@ -29,7 +29,7 @@
 
 #include <sys/cdefs.h>
 #if defined(__RCSID)
-__RCSID("$Id: ipwctl.c,v 1.1 2004/08/23 11:46:18 lukem Exp $");
+__RCSID("$Id: ipwctl.c,v 1.1.1.1 2004/08/23 11:46:18 lukem Exp $");
 #else
 #ifndef lint
 static char rcsid[] = "Id: ipwctl.c,v 1.1.2.1 2004/08/19 16:24:50 damien Exp ";
