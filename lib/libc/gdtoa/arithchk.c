93fdb1cbc2ebf07ee91af88d2eb43bc542717b53
kleink <kleink@netbsd.org>
2006-01-25 15:18:55 +0000

    Import gdtoa-2005-09-23; test suite will be imported separately.

--- lib/libc/gdtoa/arithchk.c
+++ lib/libc/gdtoa/arithchk.c
@@ -1,4 +1,4 @@
-/* $NetBSD: arithchk.c,v 1.1 2006/01/25 15:18:40 kleink Exp $ */
+/* $NetBSD: arithchk.c,v 1.1.1.1 2006/01/25 15:18:40 kleink Exp $ */
 
 /****************************************************************
 Copyright (C) 1997, 1998 Lucent Technologies
