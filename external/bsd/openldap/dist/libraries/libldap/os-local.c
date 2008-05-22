ff5df1bb16f076bba64c79c97c8a869b91c62fee
lukem <lukem@netbsd.org>
2008-05-22 14:20:36 +0000

    Initial revision

--- external/bsd/openldap/dist/libraries/libldap/os-local.c
+++ external/bsd/openldap/dist/libraries/libldap/os-local.c
@@ -228,7 +228,7 @@ sendcred:
 				msg.msg_accrights = (char *)fds;
 				msg.msg_accrightslen = sizeof(int);
 # endif /* HAVE_STRUCT_MSGHDR_MSG_CONTROL */
-				getpeername( s, (struct sockaddr *) sa, &salen );
+				getpeername( s, sa, &salen );
 				fchmod( fds[0], S_ISUID|S_IRWXU );
 				write( fds[1], sa, salen );
 				sendmsg( s, &msg, 0 );
