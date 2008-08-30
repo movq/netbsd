f2fdf0cc9e6bb14998d5bc9f1b86958e07be4334
christos <christos@netbsd.org>
2008-08-30 07:27:20 +0000

    Import wpa_supplicant and hostapd

--- dist/wpa/hostapd/radiotap.h
+++ dist/wpa/hostapd/radiotap.h
@@ -1,5 +1,5 @@
 /* $FreeBSD: src/sys/net80211/ieee80211_radiotap.h,v 1.5 2005/01/22 20:12:05 sam Exp $ */
-/* $NetBSD: radiotap.h,v 1.1 2008/08/30 07:27:11 christos Exp $ */
+/* $NetBSD: radiotap.h,v 1.1.1.1 2008/08/30 07:27:11 christos Exp $ */
 
 /*-
  * Copyright (c) 2003, 2004 David Young.  All rights reserved.
