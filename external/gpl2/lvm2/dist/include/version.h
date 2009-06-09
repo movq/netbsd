fdbba9432cd6127ea6f1201a7f3ffe31a4cb59e8
cvs2git <cvs2git@openbsd.org>
2009-06-09 00:50:59 +0000

    Initialize CVS branch agc-netpgp-standalone
    The per-file CVS branch roots do not exactly match a reconstructed source-branch commit.

--- external/gpl2/lvm2/dist/include/version.h
+++
@@ -1,5 +0,0 @@
-/*	$NetBSD: version.h,v 1.1.1.2 2009/02/18 11:16:49 haad Exp $	*/
-
-#ifndef _LVM_VERSION_H
-#define LVM_VERSION "2.02.44-cvs (02-17-09)"
-#endif
--- sys/dev/ic/acpipmtimer.h
+++
@@ -1,6 +0,0 @@
-/* $NetBSD: acpipmtimer.h,v 1.2 2009/05/12 14:25:17 cegger Exp $ */
-
-int acpipmtimer_attach(device_t,
-		       bus_space_tag_t, bus_space_handle_t, bus_size_t, int);
-#define ACPIPMT_32BIT 1
-#define ACPIPMT_BADLATCH 2
