19a68233155e683e0b1d31e19a617892b0fc9772
adam <adam@netbsd.org>
2010-11-21 17:06:08 +0000

    Imported openssh-5.6

--- crypto/external/bsd/openssh/dist/pkcs11.h
+++ crypto/external/bsd/openssh/dist/pkcs11.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: pkcs11.h,v 1.1 2010/11/21 17:05:52 adam Exp $	*/
+/*	$NetBSD: pkcs11.h,v 1.1.1.1 2010/11/21 17:05:52 adam Exp $	*/
 /* $OpenBSD: pkcs11.h,v 1.2 2010/02/24 06:12:53 djm Exp $ */
 /* pkcs11.h
    Copyright 2006, 2007 g10 Code GmbH
--- crypto/external/bsd/openssh/dist/roaming_serv.c
+++ crypto/external/bsd/openssh/dist/roaming_serv.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: roaming_serv.c,v 1.1 2010/11/21 17:05:53 adam Exp $	*/
+/*	$NetBSD: roaming_serv.c,v 1.1.1.1 2010/11/21 17:05:53 adam Exp $	*/
 /* $OpenBSD: roaming_serv.c,v 1.1 2009/10/24 11:18:23 andreas Exp $ */
 /*
  * Copyright (c) 2004-2009 AppGate Network Security AB
