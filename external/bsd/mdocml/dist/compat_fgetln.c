178f0a6bb701fad79af1016da3b3452cc1bd8801
joerg <joerg@netbsd.org>
2012-01-30 16:44:20 +0000

    Merge mdocml state from 2012-01-30 for various relaxed checks in the parser to allow it to pass almost all man pages in pkgsrc.

--- external/bsd/mdocml/dist/compat_fgetln.c
+++ external/bsd/mdocml/dist/compat_fgetln.c
@@ -8,7 +8,7 @@ int dummy;
 
 #else
 
-/*	$NetBSD: compat_fgetln.c,v 1.1 2012/01/30 16:44:03 joerg Exp $	*/
+/*	$NetBSD: compat_fgetln.c,v 1.1.1.1 2012/01/30 16:44:03 joerg Exp $	*/
 
 /*-
  * Copyright (c) 1998 The NetBSD Foundation, Inc.
