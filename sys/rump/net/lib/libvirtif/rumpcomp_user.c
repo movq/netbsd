0c3edde277aacc426b89d19dd6370df28be3c78b
cvs2git <cvs2git@openbsd.org>
2013-07-17 03:16:30 +0000

    Initialize CVS branch rmind-smpnet
    The per-file CVS branch roots do not exactly match a reconstructed source-branch commit.

--- sys/rump/net/lib/libvirtif/rumpcomp_user.c
+++ sys/rump/net/lib/libvirtif/rumpcomp_user.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rumpcomp_user.c,v 1.8 2013/07/04 11:46:51 pooka Exp $	*/
+/*	$NetBSD: rumpcomp_user.c,v 1.6 2013/05/20 10:24:26 pooka Exp $	*/
 
 /*
  * Copyright (c) 2013 Antti Kantee.  All Rights Reserved.
@@ -36,7 +36,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #ifdef __linux__
 #include <net/if.h>
@@ -44,9 +43,6 @@
 #endif
 
 #include <rump/rumpuser_component.h>
-
-#include "if_virt.h"
-#include "rumpcomp_user.h"
 
 struct virtif_user {
 	int viu_fd;
@@ -97,7 +93,7 @@ opentapdev(int devnum)
 }
 
 int
-VIFHYPER_CREATE(int devnum, struct virtif_user **viup)
+rumpcomp_virtif_create(int devnum, struct virtif_user **viup)
 {
 	struct virtif_user *viu = NULL;
 	void *cookie;
@@ -128,7 +124,7 @@ VIFHYPER_CREATE(int devnum, struct virtif_user **viup)
 }
 
 void
-VIFHYPER_SEND(struct virtif_user *viu,
+rumpcomp_virtif_send(struct virtif_user *viu,
 	struct iovec *iov, size_t iovlen)
 {
 	void *cookie = rumpuser_component_unschedule();
@@ -142,7 +138,7 @@ VIFHYPER_SEND(struct virtif_user *viu,
 /* how often to check for interface going south */
 #define POLLTIMO_MS 10
 int
-VIFHYPER_RECV(struct virtif_user *viu,
+rumpcomp_virtif_recv(struct virtif_user *viu,
 	void *data, size_t dlen, size_t *rcv)
 {
 	void *cookie = rumpuser_component_unschedule();
@@ -154,11 +150,8 @@ VIFHYPER_RECV(struct virtif_user *viu,
 	pfd.events = POLLIN;
 
 	for (;;) {
-		if (viu->viu_dying) {
-			rv = 0;
-			*rcv = 0;
+		if (viu->viu_dying)
 			break;
-		}
 
 		prv = poll(&pfd, 1, POLLTIMO_MS);
 		if (prv == 0)
@@ -187,7 +180,7 @@ VIFHYPER_RECV(struct virtif_user *viu,
 #undef POLLTIMO_MS
 
 void
-VIFHYPER_DYING(struct virtif_user *viu)
+rumpcomp_virtif_dying(struct virtif_user *viu)
 {
 
 	/* no locking necessary.  it'll be seen eventually */
@@ -195,7 +188,7 @@ VIFHYPER_DYING(struct virtif_user *viu)
 }
 
 void
-VIFHYPER_DESTROY(struct virtif_user *viu)
+rumpcomp_virtif_destroy(struct virtif_user *viu)
 {
 	void *cookie = rumpuser_component_unschedule();
 
