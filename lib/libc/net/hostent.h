2ff6d09d8570d5f6dd5f75216e8775747fbc1693
bouyer <bouyer@netbsd.org>
2013-12-18 22:39:32 +0000

    file hostent.h was added on branch netbsd-6 on 2013-12-18 22:39:32 +0000

--- lib/libc/net/hostent.h
+++ lib/libc/net/hostent.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: hostent.h,v 1.2 2013/08/27 09:56:12 christos Exp $	*/
+/*	$NetBSD: hostent.h,v 1.2.4.2 2013/12/18 22:39:32 bouyer Exp $	*/
 
 /*-
  * Copyright (c) 2013 The NetBSD Foundation, Inc.
