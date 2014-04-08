460943dae80120c19ecc968ba5941b313fbd9d3f
msaitoh <msaitoh@netbsd.org>
2014-04-08 11:58:57 +0000

    file mkpc was added on branch netbsd-6-0 on 2014-04-08 11:58:49 +0000

--- crypto/external/bsd/openssl/pkgconfig.mk
+++ crypto/external/bsd/openssl/pkgconfig.mk
@@ -1,6 +1,4 @@
-# $NetBSD: pkgconfig.mk,v 1.5 2014/04/07 17:40:49 apb Exp $
-
-.include <bsd.own.mk>
+# $NetBSD: pkgconfig.mk,v 1.5.6.2 2014/04/08 11:58:49 msaitoh Exp $
 
 FILESDIR=/usr/lib/pkgconfig
 .for pkg in ${PKGCONFIG}
@@ -8,5 +6,5 @@ FILES+=${pkg}.pc
 FILESBUILD_${pkg}.pc=yes
 
 ${pkg}.pc: ${.CURDIR}/../../mkpc
-	${HOST_SH} ${.ALLSRC} ${OPENSSLSRC}/crypto ${.TARGET} > ${.TARGET}
+	${.ALLSRC} ${OPENSSLSRC}/crypto ${.TARGET} > ${.TARGET}
 .endfor
