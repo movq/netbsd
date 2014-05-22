a61ab307f37d52973cc45775dc02168011285655
yamt <yamt@netbsd.org>
2014-05-22 16:00:51 +0000

    sync with head.
    for a reference, the tree before this commit was tagged
    as yamt-pagecache-tag8.
    
    this commit was splitted into small chunks to avoid
    a limitation of cvs.  ("Protocol error: too many arguments")

--- external/gpl3/gdb/dist/gdb/testsuite/gdb.base/infoline.c
+++ external/gpl3/gdb/dist/gdb/testsuite/gdb.base/infoline.c
@@ -1,6 +1,6 @@
-/* This testcase is part of GDB, the GNU debugger.
+/* Copyright 2011-2013 Free Software Foundation, Inc.
 
-   Copyright 1997-2013 Free Software Foundation, Inc.
+   This file is part of GDB.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -15,14 +15,16 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
-#include <stdio.h>
+int twice (int i) { /* THIS LINE */
+  /* We purposefully put the return type, function prototype and
+     opening curly brace on the same line, in an effort to make sure
+     that the function prologue would be associated to that line.  */
+  return 2 * i;
+}
 
-#ifdef PROTOTYPES
-int main (void)
-#else
-main()
-#endif
+int
+main (void)
 {
-  printf("Hello from vforked-prog...\n");
+  int t = twice (1);
   return 0;
 }
--- external/gpl3/gdb/dist/gdb/testsuite/gdb.base/interp.c
+++ external/gpl3/gdb/dist/gdb/testsuite/gdb.base/interp.c
@@ -1,6 +1,6 @@
-/* This testcase is part of GDB, the GNU debugger.
+/* This test program is part of GDB, the GNU debugger.
 
-   Copyright 1997-2013 Free Software Foundation, Inc.
+   Copyright 2011-2013 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -13,16 +13,11 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
-#include <stdio.h>
-
-#ifdef PROTOTYPES
-int main (void)
-#else
-main()
-#endif
+int
+main (int argc, const char **argv)
 {
-  printf("Hello from vforked-prog...\n");
   return 0;
 }
