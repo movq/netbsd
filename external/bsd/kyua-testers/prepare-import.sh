364874d9b5c382782edc297fed39af0b09fd8944
yamt <yamt@netbsd.org>
2014-05-22 15:45:39 +0000

    file AUTHORS was added on branch yamt-pagecache on 2014-05-22 15:45:38 +0000

--- external/bsd/kyua-testers/prepare-import.sh
+++ external/bsd/kyua-testers/prepare-import.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $NetBSD: prepare-import.sh,v 1.1 2013/02/19 06:04:42 jmmv Exp $
+# $NetBSD: prepare-import.sh,v 1.1.12.2 2014/05/22 15:45:38 yamt Exp $
 #
 # Use this script to recreate the 'dist' subdirectory from a newly released
 # distfile.  The script takes care of unpacking the distfile, removing any
