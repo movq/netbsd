3d70822722980b9844fcf5d166c16743b122b329
msaitoh <msaitoh@netbsd.org>
2014-07-09 16:04:13 +0000

    file t10.in was added on branch netbsd-5-2 on 2014-07-09 16:04:13 +0000

--- libexec/httpd/printenv.lua
+++ libexec/httpd/printenv.lua
@@ -1,4 +1,4 @@
--- $NetBSD: printenv.lua,v 1.2 2014/01/02 08:21:38 mrg Exp $
+-- $NetBSD: printenv.lua,v 1.2.14.2 2014/07/09 16:04:13 msaitoh Exp $
 
 -- this small Lua script demonstrates the use of Lua in (bozo)httpd
 -- it will simply output the "environment"
