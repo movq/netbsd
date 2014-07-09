754f545b5b397bfd78b3f6db2154d06a618ece14
msaitoh <msaitoh@netbsd.org>
2014-07-09 09:47:11 +0000

    file netbsd_queue.h was added on branch netbsd-6-0 on 2014-07-09 09:47:11 +0000

--- libexec/httpd/printenv.lua
+++ libexec/httpd/printenv.lua
@@ -1,4 +1,4 @@
--- $NetBSD: printenv.lua,v 1.2 2014/01/02 08:21:38 mrg Exp $
+-- $NetBSD: printenv.lua,v 1.2.10.2 2014/07/09 09:47:11 msaitoh Exp $
 
 -- this small Lua script demonstrates the use of Lua in (bozo)httpd
 -- it will simply output the "environment"
