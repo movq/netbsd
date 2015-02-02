fa8c462f6a3cb27b841675ed98811eed8efc7ffa
lneto <lneto@netbsd.org>
2015-02-02 02:01:18 +0000

    imported Lua 5.3.0

--- external/mit/lua/dist/src/lprefix.h
+++ external/mit/lua/dist/src/lprefix.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: lprefix.h,v 1.1 2015/02/02 02:01:13 lneto Exp $	*/
+/*	$NetBSD: lprefix.h,v 1.1.1.1 2015/02/02 02:01:13 lneto Exp $	*/
 
 /*
 ** Id: lprefix.h,v 1.2 2014/12/29 16:54:13 roberto Exp 
