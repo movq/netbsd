43d03769e7059470c47910877b90d51bf692da39
snj <snj@netbsd.org>
2015-06-08 20:33:21 +0000

    file Makefile.in was added on branch netbsd-7 on 2015-06-08 20:33:20 +0000

--- external/bsd/openpam/dist/lib/libpam/openpam_readlinev.c
+++ external/bsd/openpam/dist/lib/libpam/openpam_readlinev.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: openpam_readlinev.c,v 1.2 2014/10/24 18:17:56 christos Exp $	*/
+/*	$NetBSD: openpam_readlinev.c,v 1.2.2.2 2015/06/08 20:33:20 snj Exp $	*/
 
 /*-
  * Copyright (c) 2012 Dag-Erling Smørgrav
@@ -36,7 +36,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: openpam_readlinev.c,v 1.2 2014/10/24 18:17:56 christos Exp $");
+__RCSID("$NetBSD: openpam_readlinev.c,v 1.2.2.2 2015/06/08 20:33:20 snj Exp $");
 
 #include <errno.h>
 #include <stdio.h>
