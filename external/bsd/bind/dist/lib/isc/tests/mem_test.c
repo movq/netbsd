82faf7cc169e8afb0e1f1d1ffe97f15b639e52b6
christos <christos@netbsd.org>
2015-07-08 15:38:10 +0000

    Import bind 9.10.2-P2

--- external/bsd/bind/dist/lib/isc/tests/mem_test.c
+++ external/bsd/bind/dist/lib/isc/tests/mem_test.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: mem_test.c,v 1.1 2015/07/08 15:38:05 christos Exp $	*/
+/*	$NetBSD: mem_test.c,v 1.1.1.1 2015/07/08 15:38:05 christos Exp $	*/
 
 /*
  * Copyright (C) 2015  Internet Systems Consortium, Inc. ("ISC")
