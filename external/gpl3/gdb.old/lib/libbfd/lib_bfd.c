63ff78c539348f60cda1bf0375b37dcdfb9877f5
christos <christos@netbsd.org>
2015-08-15 09:29:34 +0000

    import the head of gdb in gdb.old in preparation for the new gdb import.

--- external/gpl3/gdb.old/lib/libbfd/lib_bfd.c
+++ external/gpl3/gdb.old/lib/libbfd/lib_bfd.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: lib_bfd.c,v 1.1 2015/08/15 09:29:28 christos Exp $	*/
+/*	$NetBSD: lib_bfd.c,v 1.1.1.1 2015/08/15 09:29:28 christos Exp $	*/
 
 /* ELF systems need this to avoid the "wrong" libbfd.so. */
 #include "libbfd.c"
