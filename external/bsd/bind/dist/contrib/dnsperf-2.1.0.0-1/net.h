a4746cec8b6a5164f92feefdf438ece933b72eac
martin <martin@netbsd.org>
2016-10-14 12:01:36 +0000

    file openssl-0.9.8zh-patch was added on branch netbsd-7 on 2016-10-14 12:01:11 +0000

--- external/bsd/bind/dist/contrib/dnsperf-2.1.0.0-1/net.h
+++ external/bsd/bind/dist/contrib/dnsperf-2.1.0.0-1/net.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: net.h,v 1.1.1.1 2016/05/26 15:45:46 christos Exp $	*/
+/*	$NetBSD: net.h,v 1.1.1.1.6.2 2016/10/14 12:01:24 martin Exp $	*/
 
 /*
  * Copyright (C) 2000, 2001  Nominum, Inc.
--- external/bsd/bind/dist/lib/dns/rdata/generic/csync_62.c
+++ external/bsd/bind/dist/lib/dns/rdata/generic/csync_62.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: csync_62.c,v 1.1.1.1 2016/05/26 15:45:51 christos Exp $	*/
+/*	$NetBSD: csync_62.c,v 1.1.1.1.6.2 2016/10/14 12:01:29 martin Exp $	*/
 
 /*
  * Copyright (C) 2015  Internet Systems Consortium, Inc. ("ISC")
