c862dd9d4ac6316a54296fb6bc9b72e9f84e863e
snj <snj@netbsd.org>
2016-12-06 06:50:23 +0000

    file xp.c was added on branch netbsd-7 on 2016-12-06 06:50:23 +0000

--- sys/arch/luna68k/dev/xp.c
+++ sys/arch/luna68k/dev/xp.c
@@ -1,4 +1,4 @@
-/* $NetBSD: xp.c,v 1.1 2016/12/03 17:38:02 tsutsui Exp $ */
+/* $NetBSD: xp.c,v 1.1.4.2 2016/12/06 06:50:23 snj Exp $ */
 
 /*-
  * Copyright (c) 2016 Izumi Tsutsui.  All rights reserved.
@@ -29,7 +29,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: xp.c,v 1.1 2016/12/03 17:38:02 tsutsui Exp $");
+__KERNEL_RCSID(0, "$NetBSD: xp.c,v 1.1.4.2 2016/12/06 06:50:23 snj Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
