0a4583e4720683e837a13199b6b2efe0f10489a2
martin <martin@netbsd.org>
2017-03-20 10:56:59 +0000

    file libssl_compat.h was added on branch netbsd-7-1 on 2017-03-20 10:56:54 +0000

--- external/bsd/ntp/dist/include/libssl_compat.h
+++ external/bsd/ntp/dist/include/libssl_compat.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: libssl_compat.h,v 1.1.1.1 2016/11/22 01:34:58 christos Exp $	*/
+/*	$NetBSD: libssl_compat.h,v 1.1.1.1.8.2 2017/03/20 10:56:54 martin Exp $	*/
 
 /*
  * libssl_compat.h -- OpenSSL v1.1 compatibility shims
