0a7bee85862f986ee0741db0e3ce236b9c0dafaa
martin <martin@netbsd.org>
2017-03-20 10:59:50 +0000

    file libssl_compat.h was added on branch netbsd-7 on 2017-03-20 10:59:44 +0000

--- external/bsd/ntp/dist/sntp/unity/ulib_teardown.c
+++ external/bsd/ntp/dist/sntp/unity/ulib_teardown.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ulib_teardown.c,v 1.1.1.1 2016/11/22 01:35:10 christos Exp $	*/
+/*	$NetBSD: ulib_teardown.c,v 1.1.1.1.10.2 2017/03/20 10:59:49 martin Exp $	*/
 
 /* default / lib implementation of 'tearDown()'
  *
