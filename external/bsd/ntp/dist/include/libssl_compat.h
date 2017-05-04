c977a8a4895a0aebb8b700db2c43571ffeae3a8d
snj <snj@netbsd.org>
2017-05-04 06:04:06 +0000

    file sntp_problemtests.m4 was added on branch netbsd-6 on 2017-05-04 06:04:03 +0000

--- external/bsd/ntp/dist/include/libssl_compat.h
+++ external/bsd/ntp/dist/include/libssl_compat.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: libssl_compat.h,v 1.1.1.2 2017/04/13 19:17:26 christos Exp $	*/
+/*	$NetBSD: libssl_compat.h,v 1.1.1.2.8.2 2017/05/04 06:03:55 snj Exp $	*/
 
 /*
  * libssl_compat.h -- OpenSSL v1.1 compatibility shims
--- external/bsd/ntp/dist/libntp/libssl_compat.c
+++ external/bsd/ntp/dist/libntp/libssl_compat.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: libssl_compat.c,v 1.1.1.2 2017/04/13 19:17:27 christos Exp $	*/
+/*	$NetBSD: libssl_compat.c,v 1.1.1.2.8.2 2017/05/04 06:03:56 snj Exp $	*/
 
 /*
  * libssl_compat.c -- OpenSSL v1.1 compatibility functions
