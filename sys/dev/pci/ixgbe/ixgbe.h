aba2f8356629718a10d143223ac26bd4b0230d1d
cvs2git <cvs2git@openbsd.org>
2018-03-06 04:45:04 +0000

    Initialize CVS branch pgoyette-compat
    The per-file CVS branch roots do not exactly match a reconstructed source-branch commit.

--- sys/dev/pci/ixgbe/ixgbe.h
+++ sys/dev/pci/ixgbe/ixgbe.h
@@ -1,4 +1,4 @@
-/* $NetBSD: ixgbe.h,v 1.33 2018/03/06 03:47:23 msaitoh Exp $ */
+/* $NetBSD: ixgbe.h,v 1.32 2018/03/02 10:19:20 knakahara Exp $ */
 
 /******************************************************************************
   SPDX-License-Identifier: BSD-3-Clause
@@ -332,7 +332,6 @@ struct ix_queue {
 	int              busy;
 	struct tx_ring   *txr;
 	struct rx_ring   *rxr;
-	struct work      wq_cookie;
 	void             *que_si;
 	struct evcnt     irqs;
 	char             namebuf[32];
@@ -340,6 +339,8 @@ struct ix_queue {
 
 	kmutex_t         im_mtx;	/* lock for im_nest and this queue's EIMS/EIMC bit */
 	int              im_nest;
+
+	struct work      wq_cookie;
 };
 
 /*
@@ -362,7 +363,6 @@ struct tx_ring {
 	ixgbe_dma_tag_t		*txtag;
 	char			mtx_name[16];
 	pcq_t			*txr_interq;
-	struct work		wq_cookie;
 	void			*txr_si;
 
 	/* Flow Director */
@@ -376,6 +376,8 @@ struct tx_ring {
 	struct evcnt		no_desc_avail;
 	struct evcnt		total_packets;
 	struct evcnt		pcq_drops;
+
+	struct work		wq_cookie;
 };
 
 
