6f65e8be327587c9e63000488db0205b9cd69c5e
martin <martin@netbsd.org>
2018-07-28 13:20:46 +0000

    file Makefile was added on branch netbsd-7 on 2018-07-28 13:20:46 +0000

--- external/bsd/dhcpcd/include/config.h
+++ external/bsd/dhcpcd/include/config.h
@@ -1,19 +1,18 @@
 /* netbsd */
-#define SYSCONFDIR	"/etc"
-#define SBINDIR		"/sbin"
-#define LIBDIR		"/lib"
-#define LIBEXECDIR	"/libexec"
-#define DBDIR		"/var/db/dhcpcd"
-#define RUNDIR		"/var/run"
-#define HAVE_IFAM_PID
-#define HAVE_IFAM_ADDRFLAGS
-#define HAVE_IFADDRS_ADDRFLAGS
-#define HAVE_UTIL_H
-#define HAVE_SETPROCTITLE
-#define HAVE_SYS_QUEUE_H
-#define HAVE_REALLOCARRAY
-#define HAVE_KQUEUE
-#define HAVE_KQUEUE1
-#define HAVE_SYS_BITOPS_H
-#define HAVE_MD5_H
-#define SHA2_H		<sha2.h>
+#define	SYSCONFDIR		"/etc"
+#define	SBINDIR			"/sbin"
+#define	LIBDIR			"/lib"
+#define	LIBEXECDIR		"/libexec"
+#define	DBDIR			"/var/db/dhcpcd"
+#define	RUNDIR			"/var/run"
+#include			"compat/pidfile.h"
+#define	HAVE_SETPROCTITLE
+#define	HAVE_SYS_QUEUE_H
+#include			"compat/reallocarray.h"
+#define	HAVE_REALLOCARRAY
+#define	HAVE_KQUEUE
+#define	HAVE_KQUEUE1
+#define	HAVE_SYS_BITOPS_H
+#define	HAVE_MD5_H
+#define	SHA2_H			<sha2.h>
+#include			"compat/crypt/hmac.h"
