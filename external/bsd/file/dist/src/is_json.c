5381a03f5884047d4972e025092217c07638fc4d
christos <christos@netbsd.org>
2018-10-18 23:54:10 +0000

    2018-10-18  19:32  Christos Zoulas <christos@zoulas.com>
    	* release 5.35
    
    2018-09-10  20:38  Christos Zoulas <christos@zoulas.com>
    
    	* Add FreeBSD ELF core file support (John Baldwin)
    
    2018-08-20  18:40  Christos Zoulas <christos@zoulas.com>
    
    	* PR/30: Allow all parameter values to be set (don't treat 0 specially)
    	* handle default annotations on the softmagic match instead at the
    	  end.
    
    2018-07-25  10:17  Christos Zoulas <christos@zoulas.com>
    
    	* PR/23: Recognize JSON files
    
    2018-07-25  10:17  Christos Zoulas <christos@zoulas.com>
    
    	* PR/18: file --mime-encoding should not print mime-type
    
    2018-07-25   8:50  Christos Zoulas <christos@zoulas.com>
    
    	* release 5.34
    
    2018-06-22  16:38  Christos Zoulas <christos@zoulas.com>
    
    	* Add Quad indirect offsets
    
    2018-05-24  14:10  Christos Zoulas <christos@zoulas.com>
    
    	* Enable parsing of ELF dynamic sections to handle PIE better

--- external/bsd/file/dist/src/is_json.c
+++ external/bsd/file/dist/src/is_json.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: is_json.c,v 1.1 2018/10/18 23:54:09 christos Exp $	*/
+/*	$NetBSD: is_json.c,v 1.1.1.1 2018/10/18 23:54:09 christos Exp $	*/
 
 /*-
  * Copyright (c) 2018 Christos Zoulas
@@ -37,7 +37,7 @@
 #if 0
 FILE_RCSID("@(#)$File: is_json.c,v 1.11 2018/10/15 16:29:16 christos Exp $")
 #else
-__RCSID("$NetBSD: is_json.c,v 1.1 2018/10/18 23:54:09 christos Exp $");
+__RCSID("$NetBSD: is_json.c,v 1.1.1.1 2018/10/18 23:54:09 christos Exp $");
 #endif
 #endif
 
