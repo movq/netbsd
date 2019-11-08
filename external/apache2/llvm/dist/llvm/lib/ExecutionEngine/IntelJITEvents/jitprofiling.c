6221691b659279dc74a50d1b9bd2317dfff1b577
joerg <joerg@netbsd.org>
2019-11-08 14:30:17 +0000

    Import 01f3a59fb3e2542fce74c768718f594d0debd0da from the LLVM mono-repo: clang (without test/, unittests/, www/) llvm (without test/, unittests/)

--- external/apache2/llvm/dist/llvm/lib/ExecutionEngine/IntelJITEvents/jitprofiling.c
+++ external/apache2/llvm/dist/llvm/lib/ExecutionEngine/IntelJITEvents/jitprofiling.c
@@ -29,7 +29,7 @@
 
 #include "jitprofiling.h"
 
-static const char rcsid[] = "\n@(#) $Revision: 1.1 $\n";
+static const char rcsid[] = "\n@(#) $Revision: 1.1.1.1 $\n";
 
 #define DLL_ENVIRONMENT_VAR             "VS_PROFILER"
 
