cf7fee5a3efb68acf12f64038e320b6566a5dc9d
martin <martin@netbsd.org>
2019-12-08 12:57:21 +0000

    file ti_wdt.c was added on branch netbsd-9 on 2019-12-08 12:57:21 +0000

--- sys/arch/arm/ti/ti_wdt.c
+++ sys/arch/arm/ti/ti_wdt.c
@@ -1,4 +1,4 @@
-/* $NetBSD: ti_wdt.c,v 1.1 2019/11/29 20:54:00 jmcneill Exp $ */
+/* $NetBSD: ti_wdt.c,v 1.1.2.2 2019/12/08 12:57:21 martin Exp $ */
 
 /*-
  * Copyright (c) 2019 Jared McNeill <jmcneill@invisible.ca>
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ti_wdt.c,v 1.1 2019/11/29 20:54:00 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ti_wdt.c,v 1.1.2.2 2019/12/08 12:57:21 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/bus.h>
