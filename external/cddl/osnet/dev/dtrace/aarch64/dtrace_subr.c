c3ca5f7ed50181dfc282badb2fe49a89b2f686d8
martin <martin@netbsd.org>
2019-12-09 15:19:32 +0000

    file dtrace_asm.S was added on branch netbsd-9 on 2019-12-09 15:19:30 +0000

--- external/cddl/osnet/dev/dtrace/aarch64/dtrace_subr.c
+++ external/cddl/osnet/dev/dtrace/aarch64/dtrace_subr.c
@@ -28,7 +28,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: dtrace_subr.c,v 1.1 2019/12/03 22:10:56 jmcneill Exp $");
+__KERNEL_RCSID(0, "$NetBSD: dtrace_subr.c,v 1.1.2.2 2019/12/09 15:19:30 martin Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
