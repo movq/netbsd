598457cc91cdf8ab59b4e92729c5b53acb91553e
martin <martin@netbsd.org>
2023-01-23 13:36:04 +0000

    file xpm.mk was added on branch netbsd-8 on 2023-01-23 13:36:04 +0000

--- external/mit/xorg/lib/libXpm/xpm.mk
+++ external/mit/xorg/lib/libXpm/xpm.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: xpm.mk,v 1.2 2023/01/20 10:20:05 mrg Exp $
+#	$NetBSD: xpm.mk,v 1.2.2.2 2023/01/23 13:36:04 martin Exp $
 
 CPPFLAGS+=	-DHAS_SNPRINTF
 CPPFLAGS+=	-DXPM_PATH_COMPRESS=\"/usr/bin/compress\"
