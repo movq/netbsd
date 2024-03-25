b91cd06d4d797977c0b213e15c96e28083b794b1
martin <martin@netbsd.org>
2024-03-25 14:26:15 +0000

    file t_sha512trunc.c was added on branch netbsd-9 on 2024-03-25 14:26:14 +0000

--- tests/crypto/libcrypto/t_sha512trunc.c
+++ tests/crypto/libcrypto/t_sha512trunc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_sha512trunc.c,v 1.2 2024/03/15 18:10:37 riastradh Exp $	*/
+/*	$NetBSD: t_sha512trunc.c,v 1.2.4.2 2024/03/25 14:26:15 martin Exp $	*/
 
 /*-
  * Copyright (c) 2024 The NetBSD Foundation, Inc.
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t_sha512trunc.c,v 1.2 2024/03/15 18:10:37 riastradh Exp $");
+__RCSID("$NetBSD: t_sha512trunc.c,v 1.2.4.2 2024/03/25 14:26:15 martin Exp $");
 
 #include <stddef.h>
 
