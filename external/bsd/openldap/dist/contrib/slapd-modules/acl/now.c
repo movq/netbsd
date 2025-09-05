d99b845dc25c269180f3467f79d018597ad29adf
christos <christos@netbsd.org>
2025-09-05 21:09:50 +0000

    Import OpenLDAP 2.6.10 (previous was 2.5.6)
    A.1. New Features and Enhancements in 2.6
    A.1.1. New features in slapd
    slapd now supports logging directly to a file, bypassing syslog.
    
    A.1.2. New features in lloadd
    lloadd now supports additional balancing mechansims
    
    A.2. Obsolete Features Removed From 2.6
    These features were strongly deprecated in 2.5 and removed in 2.6.
    
    A.2.1. back-ndb
    The experimental and incomplete back-ndb backend was removed.
    
    See CHANGES for the complete list of bugfixes.

--- external/bsd/openldap/dist/contrib/slapd-modules/acl/now.c
+++ external/bsd/openldap/dist/contrib/slapd-modules/acl/now.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: now.c,v 1.1 2025/09/05 21:09:44 christos Exp $	*/
+/*	$NetBSD: now.c,v 1.1.1.1 2025/09/05 21:09:44 christos Exp $	*/
 
 /* $OpenLDAP$ */
 /*
--- external/bsd/openldap/dist/servers/slapd/slap-cfglog.h
+++ external/bsd/openldap/dist/servers/slapd/slap-cfglog.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: slap-cfglog.h,v 1.1 2025/09/05 21:09:46 christos Exp $	*/
+/*	$NetBSD: slap-cfglog.h,v 1.1.1.1 2025/09/05 21:09:46 christos Exp $	*/
 
 /* slap-cfglog.h - logging configuration */
 /* $OpenLDAP$ */
