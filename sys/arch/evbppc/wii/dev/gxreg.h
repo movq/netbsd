d5501698379a3a5632c26df2546f152fbfe7ec18
martin <martin@netbsd.org>
2025-10-26 13:12:29 +0000

    file gxreg.h was added on branch netbsd-10 on 2025-10-26 13:12:29 +0000

--- sys/arch/evbppc/wii/dev/gxreg.h
+++ sys/arch/evbppc/wii/dev/gxreg.h
@@ -1,4 +1,4 @@
-/* $NetBSD: gxreg.h,v 1.1 2025/10/25 15:02:56 jmcneill Exp $ */
+/* $NetBSD: gxreg.h,v 1.1.4.2 2025/10/26 13:12:29 martin Exp $ */
 
 /*-
  * Copyright (c) 2025 Jared McNeill <jmcneill@invisible.ca>
