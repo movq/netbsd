0c7cc4635c3b68440a2eb2ef3361995b809146f3
martin <martin@netbsd.org>
2025-12-18 18:17:31 +0000

    file t_dladdr.c was added on branch netbsd-11 on 2025-12-18 18:17:31 +0000

--- tests/libexec/ld.elf_so/t_dladdr.c
+++ tests/libexec/ld.elf_so/t_dladdr.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: t_dladdr.c,v 1.2 2025/12/15 02:36:47 riastradh Exp $	*/
+/*	$NetBSD: t_dladdr.c,v 1.2.2.2 2025/12/18 18:17:31 martin Exp $	*/
 
 /*-
  * Copyright (c) 2025 The NetBSD Foundation, Inc.
@@ -27,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: t_dladdr.c,v 1.2 2025/12/15 02:36:47 riastradh Exp $");
+__RCSID("$NetBSD: t_dladdr.c,v 1.2.2.2 2025/12/18 18:17:31 martin Exp $");
 
 #include <sys/mman.h>
 
