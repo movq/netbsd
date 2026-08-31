2be5b0bee930ae5c67798b304c8689d81028bc88
Mike Jones <mike@mjones.org>
2026-08-31 13:54:39 +0100

    Delete stale DRM drivers for now

--- sys/external/bsd/drm2/dist/drm/nouveau/include/nvif/cl0046.h
+++
@@ -1,8 +0,0 @@
-/*	$NetBSD: cl0046.h,v 1.2 2021/12/18 23:45:33 riastradh Exp $	*/
-
-/* SPDX-License-Identifier: MIT */
-#ifndef __NVIF_CL0046_H__
-#define __NVIF_CL0046_H__
-
-#define NV04_DISP_NTFY_CONN                                                0x01
-#endif
--- sys/external/bsd/drm2/dist/drm/nouveau/include/nvkm/engine/sec.h
+++
@@ -1,8 +0,0 @@
-/*	$NetBSD: sec.h,v 1.3 2021/12/18 23:45:33 riastradh Exp $	*/
-
-/* SPDX-License-Identifier: MIT */
-#ifndef __NVKM_SEC_H__
-#define __NVKM_SEC_H__
-#include <engine/falcon.h>
-int g98_sec_new(struct nvkm_device *, enum nvkm_subdev_type, int inst, struct nvkm_engine **);
-#endif
