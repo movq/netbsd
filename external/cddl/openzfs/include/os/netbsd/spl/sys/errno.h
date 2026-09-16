9f6df2e4a1a172d3f2c1578d2a509e374ca8e1c8
Mike Jones <mike@mjones.org>
2026-09-16 19:39:21 +0000

    Preserve NetBSD ZFS checksum errors in the OpenZFS SPL

---
+++ external/cddl/openzfs/include/os/netbsd/spl/sys/errno.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+#ifndef _NETBSD_SPL_ERRNO_H_
+#define	_NETBSD_SPL_ERRNO_H_
+
+#include_next <sys/errno.h>
+
+/* Preserve the old NetBSD ZFS port's internal checksum error number. */
+#define	ECKSUM	122
+
+#endif
