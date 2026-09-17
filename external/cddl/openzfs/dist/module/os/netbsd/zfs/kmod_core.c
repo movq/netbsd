abc53760b37d7bbcea0a8bf713d1af519a02fe1a
Mike Jones <mike@mjones.org>
2026-09-17 12:50:37 +0000

    Initialize private SPL task queues and kstats with the ZFS module

--- external/cddl/openzfs/dist/module/os/netbsd/zfs/kmod_core.c
+++ external/cddl/openzfs/dist/module/os/netbsd/zfs/kmod_core.c
@@ -2,6 +2,8 @@
 
 #include <sys/zfs_context.h>
 #include <sys/module.h>
+#include <sys/kstat.h>
+#include <sys/taskq.h>
 #include <sys/spa.h>
 #include <sys/zfs_vfsops.h>
 #include <sys/zfs_ioctl_impl.h>
@@ -34,12 +36,20 @@ zfs_modcmd(modcmd_t cmd, void *arg)
 
 	switch (cmd) {
 	case MODULE_CMD_INIT:
-		error = zfs_kmod_init();
+		error = spl_kstat_init();
 		if (error != 0)
 			return (error);
-		error = vfs_attach(&zfs_vfsops);
+		taskq_init();
+		error = zfs_kmod_init();
 		if (error != 0)
-			zfs_kmod_fini();
+			goto fini_spl;
+		error = vfs_attach(&zfs_vfsops);
+		if (error == 0)
+			return (0);
+		zfs_kmod_fini();
+fini_spl:
+		taskq_fini();
+		spl_kstat_fini();
 		return (error);
 	case MODULE_CMD_FINI:
 		if (zfs_pools_busy() || zfs_busy() || zvol_busy() || zfsdev_busy() ||
@@ -49,6 +59,8 @@ zfs_modcmd(modcmd_t cmd, void *arg)
 		if (error != 0)
 			return (error);
 		zfs_kmod_fini();
+		taskq_fini();
+		spl_kstat_fini();
 		return (0);
 	case MODULE_CMD_AUTOUNLOAD:
 		/* Pool configuration can create devices without mounting a VFS. */
